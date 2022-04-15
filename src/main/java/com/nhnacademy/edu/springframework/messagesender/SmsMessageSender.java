89f8615019b8190685d0e19f1ffa6cfc14e35b76
김훈민 <bunsung92@naver.com>
2022-04-15 16:41:27 +0900

    다형성 다형성(Polymorphism) 실습2 클래스 다이어 그램

---
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/SmsMessageSender.java
@@ -0,0 +1,9 @@
+package com.nhnacademy.edu.springframework.messagesender;
+
+public class SmsMessageSender implements MessageSender{
+
+    @Override
+    public void sendMessage(User user, String message) {
+
+    }
+}
